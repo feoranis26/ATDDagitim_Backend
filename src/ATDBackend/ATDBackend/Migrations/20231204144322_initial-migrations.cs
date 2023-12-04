1884389d3f8068f488e5aba06bdbc0217d581bd2
Tuna Erhanoğlu <tunaerhanoglu@Tunas-MacBook-Air.local>
2023-12-04 17:44:15 +0300

    Not bad anymore me fixy

---
+++ src/ATDBackend/ATDBackend/Migrations/20231204144322_initial-migrations.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+#nullable disable
+
+namespace ATDBackend.Migrations
+{
+    /// <inheritdoc />
+    public partial class initialmigrations : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Seeds",
+                columns: table => new
+                {
+                    id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    name = table.Column<string>(type: "text", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Seeds", x => x.id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Seeds");
+        }
+    }
+}
