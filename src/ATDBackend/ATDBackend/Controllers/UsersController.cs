e954edf1dcb7f7bfecadc5ff06483ab7c18fb5d1
Tunanika <70443354+Tunanika@users.noreply.github.com>
2024-03-28 12:02:54 +0300

    Add mailSendTest endpoint to UsersController.cs

--- src/ATDBackend/ATDBackend/Controllers/UsersController.cs
+++ src/ATDBackend/ATDBackend/Controllers/UsersController.cs
@@ -157,6 +157,84 @@ namespace ATDBackend.Controllers
             return Ok(user);
         }
 
+        [HttpGet("sendMail")]
+        public IActionResult mailSendTest()
+        {
+            static async Task RunAsync()
+            {
+                MailjetClient client = new MailjetClient(
+                    Environment.GetEnvironmentVariable("MJ_APIKEY_PUBLIC"),
+                    Environment.GetEnvironmentVariable("MJ_APIKEY_PRIVATE")
+                );
+                MailjetRequest request = new MailjetRequest { Resource = Send.Resource, }
+                    .Property(Send.SandboxMode, "true")
+                    .Property(
+                        Send.Messages,
+                        new JArray
+                        {
+                            new JObject
+                            {
+                                {
+                                    "From",
+                                    new JArray
+                                    {
+                                        new JObject
+                                        {
+                                            { "Email", "sehirbahceleri@gmail.com" },
+                                            { "Name", "Your Mailjet Pilot" }
+                                        }
+                                    }
+                                },
+                                {
+                                    "HTMLPart",
+                                    "<h3>Dear passenger, welcome to Mailjet!</h3><br />May the delivery force be with you!"
+                                },
+                                { "Subject", "Your email flight plan!" },
+                                {
+                                    "TextPart",
+                                    "Dear passenger, welcome to Mailjet! May the delivery force be with you!"
+                                },
+                                {
+                                    "To",
+                                    new JArray
+                                    {
+                                        new JObject
+                                        {
+                                            { "Email", "sehirbahceleri@gmail.com" },
+                                            { "Name", "sehirbahceleri" }
+                                        }
+                                    }
+                                }
+                            }
+                        }
+                    );
+                MailjetResponse response = await client.PostAsync(request);
+                if (response.IsSuccessStatusCode)
+                {
+                    Console.WriteLine(
+                        string.Format(
+                            "Total: {0}, Count: {1}\n",
+                            response.GetTotal(),
+                            response.GetCount()
+                        )
+                    );
+                    Console.WriteLine(response.GetData());
+                }
+                else
+                {
+                    Console.WriteLine(string.Format("StatusCode: {0}\n", response.StatusCode));
+                    Console.WriteLine(string.Format("ErrorInfo: {0}\n", response.GetErrorInfo()));
+                    Console.WriteLine(response.GetData());
+                    Console.WriteLine(
+                        string.Format("ErrorMessage: {0}\n", response.GetErrorMessage())
+                    );
+                }
+            }
+
+            RunAsync().Wait();
+            return Ok();
+        }
+
         [HttpPost("login")]
         public IActionResult Login([FromBody] UserDto userDto) //ONLY FOR TESTING PURPOSES
         {
