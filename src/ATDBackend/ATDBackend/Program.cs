93dde92674e8db99fb8637df788f374dd55339a5
Tunanika <70443354+Tunanika@users.noreply.github.com>
2024-03-29 10:38:48 +0300

    Application port changed to 80

--- src/ATDBackend/ATDBackend/Program.cs
+++ src/ATDBackend/ATDBackend/Program.cs
@@ -100,12 +100,11 @@ namespace ATDBackend
             {
                 app.UseSwagger();
                 app.UseSwaggerUI();
+                app.UseSwaggerUI(c =>
+                {
+                    c.SwaggerEndpoint("/v1/swagger.json", "Web API V1");
+                });
             }
-            app.UseSwagger(); //FOR DEVELOPMENT ONLY
-            app.UseSwaggerUI(c =>
-            {
-                c.SwaggerEndpoint("/v1/swagger.json", "Web API V1");
-            });
 
             app.UseHttpsRedirection();
             app.UseCors(MyAllowSpecificOrigins);
