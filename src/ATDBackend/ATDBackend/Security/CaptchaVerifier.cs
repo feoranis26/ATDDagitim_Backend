b461688c06b2d1c5acfeae0a7cde24d6855638eb
Tunanika <70443354+Tunanika@users.noreply.github.com>
2024-04-05 11:14:14 +0300

    Bug fixes to MailSender

--- src/ATDBackend/ATDBackend/Security/CaptchaVerifier.cs
+++ src/ATDBackend/ATDBackend/Security/CaptchaVerifier.cs
@@ -13,23 +13,22 @@ namespace ATDBackend.Security
             INVALID
         }
 
-
         public static async Task<CaptchaResult> VerifyCaptcha(string cliResp)
         {
             try
             {
                 HttpClient client = new HttpClient();
                 string? CaptchaSecret = Environment.GetEnvironmentVariable("CAPTCHASECRET");
-                if (CaptchaSecret == null) return CaptchaResult.ERROR;
+                if (CaptchaSecret == null)
+                    return CaptchaResult.ERROR;
 
-                object obj = new
-                {
-                    secret = CaptchaSecret,
-                    response = cliResp
-                };
+                object obj = new { secret = CaptchaSecret, response = cliResp };
 
                 var content = JsonContent.Create(obj);
-                var response = await client.PostAsync("https://www.google.com/recaptcha/api/siteverify", content);
+                var response = await client.PostAsync(
+                    "https://www.google.com/recaptcha/api/siteverify",
+                    content
+                );
 
                 dynamic json = JObject.Parse(response.Content.ReadAsStringAsync().Result);
 
@@ -37,7 +36,7 @@ namespace ATDBackend.Security
 
                 return suc ? CaptchaResult.VALID : CaptchaResult.INVALID;
             }
-            catch(Exception)
+            catch (Exception)
             {
                 return CaptchaResult.ERROR;
             }
