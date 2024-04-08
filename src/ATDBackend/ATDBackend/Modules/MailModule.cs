e0d24fa8ce6b3cb33fff215daeab07ee724feb89
BoRa <bora.simsekyilmaz@gmail.com>
2024-04-08 11:55:26 +0300

    Switched from Mailjet to default mail sender

---
+++ src/ATDBackend/ATDBackend/Modules/MailModule.cs
@@ -0,0 +1,58 @@
+﻿using Newtonsoft.Json;
+using System.Net;
+using System.Net.Mail;
+
+namespace ATDBackend.Modules
+{
+    public static class MailModule
+    {
+        public static ILogger? logger { get; set; }
+        public static IConfiguration? config { get; set; }
+
+        private static SmtpClient? Client;
+
+        private static string? username;
+
+        public static bool Initialize()
+        {
+            if (config == null || logger == null) return false;
+
+            username = Environment.GetEnvironmentVariable("MAIL_USERNAME");
+
+            Client = new SmtpClient(config.GetValue<string>("Mail:SMTPHost"))
+            {
+                Port = config.GetValue<int>("Mail:SMTPPort"),
+                Credentials = new NetworkCredential(username, Environment.GetEnvironmentVariable("MAIL_PASSWORD")),
+                EnableSsl = true
+            };
+
+            return true;
+        }
+
+        public static async Task<bool> SendMailAsync(string to, string subject, string body)
+        {
+            try
+            {
+                if (username == null || Client == null) return false;
+
+                MailMessage msg = new MailMessage(username, to)
+                {
+                    Subject = subject,
+                    Body = body
+                };
+
+                await Client.SendMailAsync(msg);
+
+                return true;
+            }
+            catch (Exception)
+            {
+                return false;
+            }
+        }
+        public static bool SendMail(string to, string subject, string body)
+        {
+            return SendMailAsync(to, subject, body).Result;
+        }
+    }
+}
