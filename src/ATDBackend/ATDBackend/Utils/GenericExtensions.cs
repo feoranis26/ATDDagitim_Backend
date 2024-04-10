08015643a3b199559e4dd7a33bfea014190418e9
BoRa <bora.simsekyilmaz@gmail.com>
2024-04-10 16:49:45 +0300

    Updated login

---
+++ src/ATDBackend/ATDBackend/Utils/GenericExtensions.cs
@@ -0,0 +1,8 @@
+﻿namespace ATDBackend.Utils
+{
+    public static class GenericExtensions
+    {
+        public static bool IsNullOrEmpty(this string value) => string.IsNullOrEmpty(value);
+        public static bool IsNullOrEmptyWithTrim(this string value) => string.IsNullOrEmpty(value.Trim());
+    }
+}
