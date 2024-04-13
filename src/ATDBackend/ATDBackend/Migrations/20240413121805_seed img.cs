ca11c95bb83a2f90c176c1cae2075bf39c16b1f9
Bora <89869983+BoRa-SY@users.noreply.github.com>
2024-04-13 15:18:25 +0300

    Created migration files

---
+++ src/ATDBackend/ATDBackend/Migrations/20240413121805_seed img.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ATDBackend.Migrations
+{
+    /// <inheritdoc />
+    public partial class seedimg : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<byte[]>(
+                name: "Image",
+                table: "Seeds",
+                type: "bytea",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "text");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Image",
+                table: "Seeds",
+                type: "text",
+                nullable: false,
+                oldClrType: typeof(byte[]),
+                oldType: "bytea");
+        }
+    }
+}
