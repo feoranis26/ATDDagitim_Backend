59b77c7e2ff876ba024588ea04b5d7d361a2ed60
Bora <89869983+BoRa-SY@users.noreply.github.com>
2024-04-14 15:45:52 +0300

    Update BasketController.cs

--- src/ATDBackend/ATDBackend/Controllers/BasketController.cs
+++ src/ATDBackend/ATDBackend/Controllers/BasketController.cs
@@ -3,6 +3,7 @@ using ATDBackend.Database.Models;
 using ATDBackend.Security.SessionSystem;
 using ATDBackend.Security;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace ATDBackend.Controllers
 {
@@ -23,13 +24,49 @@ namespace ATDBackend.Controllers
         [RequireAuth(Permission.SCHOOL_SELF_PURCHASEPRODUCT)]
         public IActionResult GetBasket()
         {
-            return Ok();
+            int? userId = (HttpContext.Items["User"] as User)?.Id;
+            if (userId == null) return Unauthorized("nouser");
+
+            var res =
+                _context.Users.Where(x => x.Id == userId)
+                .Include(x => x.BasketSeeds).ThenInclude(x => x.Seed).Select(x => new
+                {
+                    seeds = x.BasketSeeds.Select(x => new
+                    {
+                        id = x.Seed.Id,
+                        name = x.Seed.Name,
+                        price = x.Seed.Price,
+                        quantity = x.Quantity
+                    })
+                }).FirstOrDefault();
+
+            return Ok(res);
         }
 
         [HttpPost]
         [RequireAuth(Permission.SCHOOL_SELF_PURCHASEPRODUCT)]
         public IActionResult AddToBasket(int seedId, int quantity)
         {
+            int? userId = (HttpContext.Items["User"] as User)?.Id;
+            if (userId == null) return Unauthorized("nouser");
+
+            if(_context.Seeds.Find(seedId) == null) return NotFound("seednotfound");
+            if(quantity <= 0 || quantity > 200) return BadRequest("invalidquantity");
+
+
+
+            User? user = _context.Users.Where(x => x.Id == userId).Include(x => x.BasketSeeds).FirstOrDefault();
+            if(user == null) return Unauthorized("nouser");
+
+            user.BasketSeeds.Add(new BasketSeed()
+            {
+                Quantity = quantity,
+                SeedId = seedId,
+                UserId = userId.Value
+            });
+            _context.Users.Update(user);
+            _context.SaveChanges();
+
             return Ok();
         }
 
@@ -37,6 +74,26 @@ namespace ATDBackend.Controllers
         [RequireAuth(Permission.SCHOOL_SELF_PURCHASEPRODUCT)]
         public IActionResult DeleteSingleFromBasket(int seedId)
         {
+            int? userId = (HttpContext.Items["User"] as User)?.Id;
+            if (userId == null) return Unauthorized("nouser");
+
+            if (_context.Seeds.Find(seedId) == null) return NotFound("seednotfound");
+
+
+
+            User? user = _context.Users.Where(x => x.Id == userId).Include(x => x.BasketSeeds).FirstOrDefault();
+            if (user == null) return Unauthorized("nouser");
+
+            BasketSeed? bs = user.BasketSeeds.Where(x => x.SeedId == seedId).FirstOrDefault();
+            if(bs == null) return NotFound("basketseednotfound");
+
+            bool s = user.BasketSeeds.Remove(bs);
+
+            if (!s) return StatusCode(500);
+
+            _context.Users.Update(user);
+            _context.SaveChanges();
+
             return Ok();
         }
 
@@ -44,6 +101,22 @@ namespace ATDBackend.Controllers
         [RequireAuth(Permission.SCHOOL_SELF_PURCHASEPRODUCT)]
         public IActionResult DeleteAllFromBasket()
         {
+            int? userId = (HttpContext.Items["User"] as User)?.Id;
+            if (userId == null) return Unauthorized("nouser");
+
+
+
+
+            User? user = _context.Users.Where(x => x.Id == userId).Include(x => x.BasketSeeds).FirstOrDefault();
+            if (user == null) return Unauthorized("nouser");
+
+
+            user.BasketSeeds.Clear();
+
+
+            _context.Users.Update(user);
+            _context.SaveChanges();
+
             return Ok();
         }
 
