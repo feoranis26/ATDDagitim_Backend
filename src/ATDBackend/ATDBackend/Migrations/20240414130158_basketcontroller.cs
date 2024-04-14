055b118957241e7717ac39e72c1abcb191098dab
Bora <89869983+BoRa-SY@users.noreply.github.com>
2024-04-14 16:02:13 +0300

    created migration files

---
+++ src/ATDBackend/ATDBackend/Migrations/20240414130158_basketcontroller.cs
@@ -0,0 +1,62 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ATDBackend.Migrations
+{
+    /// <inheritdoc />
+    public partial class basketcontroller : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "BasketJson",
+                table: "Users");
+
+            migrationBuilder.CreateTable(
+                name: "BasketSeeds",
+                columns: table => new
+                {
+                    SeedId = table.Column<int>(type: "integer", nullable: false),
+                    UserId = table.Column<int>(type: "integer", nullable: false),
+                    Quantity = table.Column<int>(type: "integer", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_BasketSeeds", x => new { x.SeedId, x.UserId });
+                    table.ForeignKey(
+                        name: "FK_BasketSeeds_Seeds_SeedId",
+                        column: x => x.SeedId,
+                        principalTable: "Seeds",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                    table.ForeignKey(
+                        name: "FK_BasketSeeds_Users_UserId",
+                        column: x => x.UserId,
+                        principalTable: "Users",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_BasketSeeds_UserId",
+                table: "BasketSeeds",
+                column: "UserId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "BasketSeeds");
+
+            migrationBuilder.AddColumn<string>(
+                name: "BasketJson",
+                table: "Users",
+                type: "text",
+                nullable: false,
+                defaultValue: "");
+        }
+    }
+}
