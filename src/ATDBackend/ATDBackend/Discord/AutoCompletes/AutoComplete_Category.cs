78381ba22887bb414fb1af4c3e20d58c60499cfd
Bora <89869983+BoRa-SY@users.noreply.github.com>
2024-04-15 00:14:16 +0300

    Update AutoComplete_Category.cs

--- src/ATDBackend/ATDBackend/Discord/AutoCompletes/AutoComplete_Category.cs
+++ src/ATDBackend/ATDBackend/Discord/AutoCompletes/AutoComplete_Category.cs
@@ -27,16 +27,10 @@ namespace ATDBackend.Discord.AutoCompletes
                 Console.WriteLine(categoryStr);
                 Dictionary<int, string> categories = new Dictionary<int, string>();
 
-                List<DiscordAutoCompleteChoice> choices = new()
-                {
-                    new DiscordAutoCompleteChoice("name", "val"),
-                    new DiscordAutoCompleteChoice("name2", "val2")
-                };
+                List<DiscordAutoCompleteChoice> choices = new();
 
-                return choices;
-
-                if (categoryStr.IsNullOrEmpty()) (await dbContext.Categories.ToListAsync()).ForEach(x => choices.Add(new(x.CategoryName, x.Id)));
-                else (await dbContext.Categories.Where(x => x.CategoryName.ToLower().Contains(categoryStr)).ToListAsync()).ForEach(x => choices.Add(new(x.CategoryName, x.Id)));
+                if (categoryStr.IsNullOrEmpty()) (await dbContext.Categories.ToListAsync()).ForEach(x => choices.Add(new(x.CategoryName, x.Id.ToString())));
+                else (await dbContext.Categories.Where(x => x.CategoryName.ToLower().Contains(categoryStr)).ToListAsync()).ForEach(x => choices.Add(new(x.CategoryName, x.Id.ToString())));
 
                 Console.WriteLine($"LEN: {choices.Count} // {string.Join(" / ", choices.Select(x => x.Name))}");
                 return choices;
