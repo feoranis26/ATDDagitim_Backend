0fe0ff0b7fc169fba5e46659f899952efdcca93b
Bora <89869983+BoRa-SY@users.noreply.github.com>
2024-04-14 18:54:51 +0300

    Create Commands_Misc.cs

---
+++ src/ATDBackend/ATDBackend/Discord/Commands/Commands_Misc.cs
@@ -0,0 +1,16 @@
+﻿using ATDBackend.Discord.Extensions;
+using DSharpPlus.SlashCommands;
+
+namespace ATDBackend.Discord.Commands
+{
+    public class Commands_Misc : ApplicationCommandModule
+    {
+        [SlashCommand("ping", "Replies with pong!")]
+        public async Task Ping(InteractionContext ctx)
+        {
+            await ctx.DeferAsync();
+
+            await ctx.EditResponseAsync("PONGGGGG");
+        }
+    }
+}
