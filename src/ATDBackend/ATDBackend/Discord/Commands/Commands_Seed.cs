f76dcce84d2d1963b88bf9e4d9077d309d9a2cbe
Bora <89869983+BoRa-SY@users.noreply.github.com>
2024-04-14 19:37:27 +0300

    Update Commands_Seed.cs

--- src/ATDBackend/ATDBackend/Discord/Commands/Commands_Seed.cs
+++ src/ATDBackend/ATDBackend/Discord/Commands/Commands_Seed.cs
@@ -7,14 +7,8 @@ using DSharpPlus.SlashCommands;
 namespace ATDBackend.Discord.Commands
 {
     [SlashCommandGroup("seed", "commands related to seed operations,")]
-    public class Commands_Seed(
-        ILogger<AuthController> logger,
-        IConfiguration configuration,
-        AppDBContext context) : ApplicationCommandModule
+    public class Commands_Seed() : ApplicationCommandModule
     {
-        private readonly IConfiguration _configuration = configuration;
-        private readonly ILogger<AuthController> _logger = logger;
-        private readonly AppDBContext _context = context;
 
 
         [SlashCommand("add", "Add a seed to the database")]
