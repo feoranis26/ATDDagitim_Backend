04024023c0da134f3a834071f79a8289f19ffcfb
Bora <89869983+BoRa-SY@users.noreply.github.com>
2024-04-15 06:17:56 +0300

    Update Commands_Seed.cs

--- src/ATDBackend/ATDBackend/Discord/Commands/Commands_Seed.cs
+++ src/ATDBackend/ATDBackend/Discord/Commands/Commands_Seed.cs
@@ -19,18 +19,79 @@ namespace ATDBackend.Discord.Commands
         private readonly AppDBContext dbContext = appDbContext;
 
         [SlashCommand("add", "Creates a new seed")]
-        public async Task Ping(InteractionContext ctx, [Autocomplete(typeof(AutoComplete_Category))][Option("cateogry", "Seed category", true)] string categoryid)
+        public async Task Ping(InteractionContext ctx,
+            [Option("name", "Name of the seed")] string name,
+            [Option("description", "Description of the seed")] string description,
+            [Autocomplete(typeof(AutoComplete_Category))][Option("cateogry", "Seed category", true)] string categoryid_str,
+            [Option("image", "Image of the seed")] byte[] image,
+            [Option("price", "Price of the seed")] string price_str,
+            [Option("is_active", "Whether if the seed is active or not")] bool isActive,
+            [Autocomplete(typeof(AutoComplete_School))][Option("contributor_school", "Contributor school of the seed")] string cschoolid_str,
+            [Option("stock", "Stock of the seed")] string stock_str,
+            [Option("id", "id")] string id_str
+            )
         {
-            try
-            {
-                await ctx.DeferAsync();
 
-                await ctx.EditResponseAsync(categoryid.ToString());
-            }
-            catch(Exception ex)
+            await ctx.DeferAsync();
+
+            if (!int.TryParse(price_str, out int price))
             {
-                await ctx.EditResponseAsync(ex.Message);
+                await ctx.EditResponseAsync("Invalid price");
+                return;
             }
+
+            if (!int.TryParse(stock_str, out int stock))
+            {
+                await ctx.EditResponseAsync("Invalid stock");
+                return;
+            }
+
+            if (!int.TryParse(id_str, out int id))
+            {
+                await ctx.EditResponseAsync("Invalid id");
+                return;
+            }
+
+            if (!int.TryParse(categoryid_str, out int categoryId) || dbContext.Categories.Find(categoryId) == null)
+            {
+                await ctx.EditResponseAsync("Invalid category id");
+                return;
+            }
+
+            if (!int.TryParse(cschoolid_str, out int cschoolId) || dbContext.Schools.Find(cschoolId) == null)
+            {
+                await ctx.EditResponseAsync("Invalid school id");
+                return;
+            }
+
+
+
+
+            Seed seed = new Seed()
+            {
+                Name = name,
+                Description = description,
+                CategoryId = categoryId,
+                Image = image,
+                Price = price,
+                Is_active = isActive,
+                Stock = stock,
+                Date_added = DateTime.Now,
+                Id = id,
+                SeedContributors = new List<SeedContributor>()
+                {
+                    new SeedContributor()
+                    {
+                        SchoolId = cschoolId,
+                        SeedId = id
+                    }
+                }
+            };
+
+            dbContext.Seeds.Add(seed);
+            await dbContext.SaveChangesAsync();
+
+            await ctx.EditResponseAsync("Success");
         }
     }
 }
