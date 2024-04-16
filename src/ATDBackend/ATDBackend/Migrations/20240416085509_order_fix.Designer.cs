b16f6e3c76a7b7551e5e409356748fd27ac8c4d1
Tunanika <70443354+Tunanika@users.noreply.github.com>
2024-04-16 11:55:13 +0300

    order migrated

--- src/ATDBackend/ATDBackend/Migrations/20240416085509_order_fix.Designer.cs
+++ src/ATDBackend/ATDBackend/Migrations/20240416085509_order_fix.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using ATDBackend.Database.DBContexts;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 
@@ -11,9 +12,11 @@ using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 namespace ATDBackend.Migrations
 {
     [DbContext(typeof(AppDBContext))]
-    partial class AppDBContextModelSnapshot : ModelSnapshot
+    [Migration("20240416085509_order_fix")]
+    partial class order_fix
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ src/ATDBackend/ATDBackend/Migrations/20240416085509_order_fix.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ATDBackend.Migrations
+{
+    /// <inheritdoc />
+    public partial class order_fix : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Seeds",
+                table: "Orders");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<List<int>>(
+                name: "Seeds",
+                table: "Orders",
+                type: "integer[]",
+                nullable: false);
+        }
+    }
+}
